b206b172d4d4bb9bcf1c28122877920d58862248
011085 <lyamil377@gmail.com>
2024-08-01 00:12:35 -0300

    BOTONES-NUEVOS

---
+++ boton2.js
@@ -0,0 +1,11 @@
+function redirigirASitio() {
+    window.open('http://facebook.com/xznoise', '_blank');
+}
+
+function redirigirASitio() {
+    window.open('https://veinte33records.bandcamp.com/album/nada', '_blank');
+}
+
+function redirigirASitio() {
+    window.open('https://www.discogs.com/es/artist/7408225-Existenz-5', '_blank');
+}
